0c7fca9248ad1930186bb1d982749f672bc1157d
hayley.mcc@gmail.com <hayley.mcc@gmail.com>
2022-04-28 13:17:14 +0100

    Finished Monsters Rolodex using Class Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+
 import './App.css';
 
 class App extends Component {
@@ -8,18 +10,47 @@ class App extends Component {
 		super();
 
 		this.state = {
-			name: 'Hayley',
+			monsters: [],
+			searchField: '',
 		};
 	}
 
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users').then((response) =>
+			response.json().then((users) =>
+				this.setState(() => {
+					return { monsters: users };
+				})
+			)
+		);
+	}
+
+	onSearchChange = (event) => {
+		const searchField = event.target.value.toLowerCase();
+		this.setState(() => {
+			return {
+				searchField,
+			};
+		});
+	};
+
 	render() {
+		const { monsters, searchField } = this.state;
+		const { onSearchChange } = this;
+
+		const filteredMonsters = monsters.filter((monster) => {
+			return monster.name.toLowerCase().includes(searchField);
+		});
+
 		return (
 			<div className='App'>
-				<header className='App-header'>
-					<img src={logo} className='App-logo' alt='logo' />
-					<p>Hello {this.state.name}</p>
-					<button>Change Name</button>
-				</header>
+				<h1 className='app-title'>Monsters Rolodex</h1>
+				<SearchBox
+					className='monsters-search-box'
+					onChangeHandler={onSearchChange}
+					placeholder='Search Monsters'
+				/>
+				<CardList monsters={filteredMonsters} />
 			</div>
 		);
 	}
